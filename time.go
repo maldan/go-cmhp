ad48a93cc9170ed1fa5b059bc6cf4abd9125863f
Roman <blackwanted@yandex.ru>
2021-06-25 17:52:06 +0300

    Fix -- None

---
+++ time.go
@@ -0,0 +1,16 @@
+package cmhp
+
+import (
+	"strings"
+	"time"
+)
+
+func TimeFormat(t time.Time, format string) string {
+	format = strings.ReplaceAll(format, "YYYY", "2006")
+	format = strings.ReplaceAll(format, "MM", "01")
+	format = strings.ReplaceAll(format, "DD", "02")
+	format = strings.ReplaceAll(format, "HH", "15")
+	format = strings.ReplaceAll(format, "mm", "04")
+	format = strings.ReplaceAll(format, "ss", "05")
+	return t.Format(format)
+}

5b9e4a4db376989c021879b3365ac20d8fb7fe23
Roman <blackwanted@yandex.ru>
2021-07-01 23:34:36 +0300

    Fix -- Sas

--- process_windows.go
+++ process_windows.go
@@ -3,12 +3,13 @@ package cmhp
 import (
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
 func ProcessExec(args ...string) string {
 	c, b := exec.Command(args[0], args[1:]...), new(strings.Builder)
 	c.Stdout = b
-	c.SysProcAttr.HideWindow = true
+	c.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	c.Run()
 	return b.String()
 }

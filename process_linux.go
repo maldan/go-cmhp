477263cc9dbb3339a7e6fced203a15f3f6a3fc50
Roman <blackwanted@yandex.ru>
2021-07-12 13:54:48 +0300

    Fix -- Hot fix

--- process_linux.go
+++ process_linux.go
@@ -11,3 +11,9 @@ func ProcessExec(args ...string) string {
 	c.Run()
 	return b.String()
 }
+
+func ProcessCreate(args ...string) (*exec.Cmd, *strings.Builder) {
+	c, b := exec.Command(args[0], args[1:]...), new(strings.Builder)
+	c.Stdout = b
+	return c, b
+}

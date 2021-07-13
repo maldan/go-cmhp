17c082187a3ca930c25e3e338bd303ea5f43bc9b
Roman <blackwanted@yandex.ru>
2021-07-13 15:06:18 +0300

    UID generattion

---
+++ cmhp_test.go
@@ -0,0 +1,8 @@
+package cmhp
+
+/*func TestAbs(t *testing.T) {
+	for i := 0; i < 1024; i++ {
+		fmt.Println(UID(8))
+	}
+}
+*/
--- crypto.go
+++ crypto.go
@@ -1,12 +1,34 @@
 package cmhp
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
+	"math/big"
+	"os"
+	"runtime"
+	"time"
 )
 
 func HashSha256(data string) string {
 	hasher := sha256.New()
 	hasher.Write([]byte(data))
 	return base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+}
+
+func UID(size int) string {
+	a := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	out := ""
+	t := time.Now().Unix() + int64(os.Getpid()) + int64(m.TotalAlloc+m.Alloc+m.Sys)
+
+	for i := 0; i < size; i++ {
+		num, _ := rand.Int(rand.Reader, big.NewInt(1_000_000_000))
+		x := int(num.Int64() + t)
+		out += string(a[x%len(a)])
+	}
+
+	return out
 }

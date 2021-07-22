c39ab9c0ea2f88b8807d66eaed44611abb51cdda
Roman <blackwanted@yandex.ru>
2021-07-22 20:39:13 +0300

    Fix -- Linux

--- process_linux.go
+++ process_linux.go
@@ -3,11 +3,15 @@ package cmhp
 import (
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
 func ProcessExec(args ...string) string {
 	c, b := exec.Command(args[0], args[1:]...), new(strings.Builder)
 	c.Stdout = b
+	c.SysProcAttr = &syscall.SysProcAttr{
+		Noctty: true,
+	}
 	c.Run()
 	return b.String()
 }
@@ -15,5 +19,8 @@ func ProcessExec(args ...string) string {
 func ProcessCreate(args ...string) (*exec.Cmd, *strings.Builder) {
 	c, b := exec.Command(args[0], args[1:]...), new(strings.Builder)
 	c.Stdout = b
+	c.SysProcAttr = &syscall.SysProcAttr{
+		Noctty: true,
+	}
 	return c, b
 }
